bd81ca36391fb575a9205b173808c457a2b3971f
Pete Wildsmith <pete@weargoggles.co.uk>
2021-12-01 13:53:02 +0100

    day 1, part 2

--- day1.jl
+++ day1.jl
@@ -1,4 +1,7 @@
 lines = readlines("inputs/1-1.txt")
 pars(i) = parse(Int64, i)
 depths = map(pars, lines)
-print(sum(x[2] > x[1] for x=zip(depths, Iterators.drop(depths, 1))))
+println(sum(x[2] > x[1] for x=zip(depths, Iterators.drop(depths, 1))))
+
+
+println(sum(x[2] > x[1] for x=zip(depths, Iterators.drop(depths, 3))))
