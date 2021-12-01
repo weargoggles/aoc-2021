26b67cec84600a272c23169b31a2b00328c1a329
Pete Wildsmith <pete@weargoggles.co.uk>
2021-12-01 14:03:49 +0100

    golf

--- day1.jl
+++ day1.jl
@@ -1,6 +1,3 @@
-lines = readlines("inputs/1-1.txt")
-depths = map(Base.Fix1(parse, Int64), lines)
+depths = map(Base.Fix1(parse, Int64), readlines("inputs/1-1.txt"))
 println(sum(x[2] > x[1] for x=zip(depths, Iterators.drop(depths, 1))))
-
-
 println(sum(x[2] > x[1] for x=zip(depths, Iterators.drop(depths, 3))))
