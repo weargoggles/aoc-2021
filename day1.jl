a69bbfc600b2d1047576a88f35600d56392e195e
Pete Wildsmith <pete@weargoggles.co.uk>
2021-12-01 14:03:14 +0100

    use Julia's Fix instead of a closure

--- day1.jl
+++ day1.jl
@@ -1,6 +1,5 @@
 lines = readlines("inputs/1-1.txt")
-pars(i) = parse(Int64, i)
-depths = map(pars, lines)
+depths = map(Base.Fix1(parse, Int64), lines)
 println(sum(x[2] > x[1] for x=zip(depths, Iterators.drop(depths, 1))))
 
 
